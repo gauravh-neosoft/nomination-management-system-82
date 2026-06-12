c6114211dd6eb06b88cebaeb5031469d979ce745
gauravh-neosoft <gaurav.heda@neosoftmail.com>
2026-06-12 13:56:06 +0530

    feat(layout): refactor notifications matching designer specs

--- resources/views/layouts/notifications/unit-spoc-notification.blade.php
+++ resources/views/layouts/notifications/unit-spoc-notification.blade.php
@@ -1,22 +1,19 @@
 <ul
-  class="dropdown-menu dropdown-menu-end notifications rounded-4 border list-unstyled overflow-auto hide-scrollbar py-0"
->
+  class="dropdown-menu dropdown-menu-end notifications rounded-4 border list-unstyled overflow-auto hide-scrollbar py-0">
   <li
-    class="p-3 d-flex justify-content-between border-bottom notification-border bg-white sticky-top"
-  >
+    class="p-3 d-flex justify-content-between border-bottom notification-border bg-white sticky-top">
     <div class="d-flex align-items-center gap-2">
       <i class="bi bi-bell icon text-primary"></i>
-      <h3 class="card-title fw-normal m-0 fs-09">SPOC Notifications</h3>
+      <h3 class="card-title fw-normal m-0 fs-09">Notifications</h3>
     </div>
     <button
       type="button"
       class="btn btn-sm p-0 border-0 bg-transparent text-light-grey"
-      aria-label="Close notifications"
-    >
+      aria-label="Close notifications">
       <i class="bi bi-x-lg"></i>
     </button>
   </li>
-  
+
   <li class="p-3 pb-2 d-flex gap-2 border-bottom notification-border">
     <div class="icon-with-bg icon-bg-light-blue d-flex justify-content-center align-items-center">
       <i class="bi bi-shield-check text-blue"></i>
